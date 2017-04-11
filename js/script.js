6512d22927b201845c0037e7912f347e969c7b5c
guillaumepn <guillaumepn@free.fr>
2017-04-11 17:02:37 +0200

    Update JSON

--- js/script.js
+++ js/script.js
@@ -2,20 +2,28 @@ var canClick = true;
 var play;
 var canPlay = true;
 
-var json = '[{"url":"https:\/\/static.pexels.com\/photos\/292600\/pexels-photo-292600.jpeg","title":"La montagne","desc":"Elle est belle ma montagne"},{"url":"https:\/\/static.pexels.com\/photos\/192505\/pexels-photo-192505.jpeg","title":"La route","desc":"Elle est belle ma route"},{"url":"https:\/\/static.pexels.com\/photos\/66258\/staniel-cay-swimming-pig-seagull-fish-66258.jpeg","title":"La plage","desc":"Elle est belle ma page"}]'
+$.getJSON("https://www.skrzypczyk.fr/slideshow.php", function (data) {
+	var nbImages = 0;
+	$.each(data, function(key, value) {
+		$("#rail").append("<img src=\""+ value["url"] +"\" desc=\""+ value["desc"] +"\" alt=\""+ value["title"] +"\">");
+		nbImages++;
+		//console.log($("img:eq(1)").css("height"));
+	});
+	
+	$("#rail").css("width", nbImages*800);
 
-json = json.replace("\\/", "/");
+});
 
 
 function showNext(){
 	if (canClick) {
 		canClick = false;
 		$("#rail").animate({
-			marginLeft: "-=400"
+			marginLeft: "-=800"
 		}, "slow", function() {
 			var first = $("img:first");
 			$("#rail img:last-child").after(first);
-			$(this).css("margin-left", "+=400px");
+			$(this).css("margin-left", "+=800px");
 			canClick = true;
 		});
 	}
@@ -23,7 +31,7 @@ function showNext(){
 
 $("#play").click(function(){
 	if(canPlay == true){
-		play = setInterval(showNext, 1000);
+		play = setInterval(showNext, 2000);
 		canPlay = false;
 		$("#pauseOrPlay").removeClass("fa-play");
 		$("#pauseOrPlay").addClass("fa-pause");
@@ -43,9 +51,9 @@ $("#previous").click(function() {
 		canClick = false;
 		var last = $("img:last");
 		$("#rail img:first-child").before(last);
-		$("#rail").css("margin-left", "-=400px");
+		$("#rail").css("margin-left", "-=800px");
 		$("#rail").animate({
-			marginLeft: "+=400"
+			marginLeft: "+=800"
 		}, "slow", function() {
 			canClick = true;
 		});
